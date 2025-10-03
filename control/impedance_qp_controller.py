951267ab078d6f8490a3988ae8f5880a0c45d2ee
aweinhardt <wkadam9@gmail.com>
2025-10-03 18:33:04 +0200

    updates

--- control/impedance_qp_controller.py
+++ control/impedance_qp_controller.py
@@ -1,5 +1,5 @@
 import numpy as np
-import zmq
+import time
 from control.pid_ff_controller import URForceController 
 
 class JointOptimization():
@@ -7,9 +7,51 @@ class JointOptimization():
         self.robotL = robotL
         self.robotR = robotR
         self.Hz = Hz
-        self.trajectory = trajectory
+
+        #references
+        trajectory_npz=np.load(trajectory)
+        self.position_ref = trajectory_npz['position']
+        self.velocity_ref = trajectory_npz['linear_velocity']
+        self.rotation_ref = trajectory_npz['rotation_matrices']
 
     def run(self):
+        control_period = 1.0 / self.control_rate_hz
+        trajectory_index = 0
+
+        while not self.control_stop.is_set():
+            loop_start = time.perf_counter()
+            try:
+
+                # =========================== Trajectory Updates ===========================
+
+                trajectory_index += 1
+
+
+                # --- Log ---
+                self.control_data.append(
+                    {
+
+                    }
+                )
+
+
+            except Exception as e:
+                print(f"Control error: {e}")
+                try:
+                    self.speedStop()
+                except:
+                    pass
+                break
+
+            # Keep loop rate
+            elapsed = time.perf_counter() - loop_start
+            time.sleep(max(0, control_period - elapsed))
+
+        # Clean stop
+        try:
+            self.speedStop()
+        except:
+            pass
 
 class URImpedanceController(URForceController):
     def __init__(self, ip, K):
@@ -22,11 +64,11 @@ class URImpedanceController(URForceController):
         self.M = self.get_mass_matrix()
         self.Lambda= np.linalg.inv(self.J @ np.linalg.inv(self.M) @ self.J.T)
 
-        self.cotrol_data = []
+        self.cotrol_data = []   
 
     def get_D(self, K, Lambda):
         D = np.sqrt(Lambda) * np.sqrt(K) + np.sqrt(K) * np.sqrt(Lambda)
         return D
-
-
-        
+    
+    def get_wrench_desired(self, D, K, position, velocity, rotation, position_ref, velocity_ref, rotation_ref,):
+        wrench = D @ (velocity_ref - velocity) + K @ (position_ref - position)
