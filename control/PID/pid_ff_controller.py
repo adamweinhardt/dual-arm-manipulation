fb0ff0c50eecdc374cce0592f143f437bfb97449
aweinhardt <wkadam9@gmail.com>
2025-12-14 17:32:47 +0100

    final

--- control/PID/pid_ff_controller.py
+++ control/PID/pid_ff_controller.py
@@ -1626,3 +1626,137 @@ class URForceController(URController):
         np.savez_compressed(out_path, **pack)
         print(f"[SAVED] {out_path}")
         return out_path
+
+
+if __name__ == "__main__":
+    hz = 100
+    reference_force = 50
+    base_force = 12.5
+    factor = base_force / reference_force
+
+    kp_f = 0.0065 * factor
+    ki_f = 0.0001 * factor
+    kd_f = 0.001 * factor
+
+    kp_p = 1.2  # 0.5
+    ki_p = 0.00005
+    kd_p = 0.34  # 0.0025
+
+    kp_r = 1.35
+    ki_r = 0
+    kd_r = 0.1
+
+    alpha = 0.85
+    deadzone_threshold = 0.02
+    trajectory = "motion_planner/trajectories/pick_and_place.npz"
+
+    robotL = URForceController(
+        "192.168.1.33",
+        hz=hz,
+        kp_f=kp_f,
+        ki_f=ki_f,
+        kd_f=kd_f,
+        kp_p=kp_p,
+        ki_p=ki_p,
+        kd_p=kd_p,
+        kp_r=kp_r,
+        ki_r=ki_r,
+        kd_r=kd_r,
+    )
+    robotR = URForceController(
+        "192.168.1.66",
+        hz=hz,
+        kp_f=kp_f,
+        ki_f=ki_f,
+        kd_f=kd_f,
+        kp_p=kp_p,
+        ki_p=ki_p,
+        kd_p=kd_p,
+        kp_r=kp_r,
+        ki_r=ki_r,
+        kd_r=kd_r,
+    )
+
+    robotL.alpha = alpha
+    robotR.alpha = alpha
+
+    try:
+        robotL.moveJ(
+            [
+                -0.9861305395709437,
+                -1.0489304226687928,
+                2.1829379240619105,
+                -2.6813165150084437,
+                -1.598926846181051,
+                3.818711757659912,
+            ]
+        )
+
+        robotL.wait_for_commands()
+        robotL.moveJ(
+            [-2.72771532, -1.40769446, 2.81887228, -3.01955523, -1.6224683, 2.31350756]
+        )
+
+        robotL.wait_for_commands()
+
+        robotR.go_home()
+        robotL.go_home()
+
+        robotR.wait_for_commands()
+        robotL.wait_for_commands()
+
+        robotR.wait_until_done()
+        robotL.wait_until_done()
+
+        robotR.go_to_approach()
+        robotL.go_to_approach()
+
+        robotR.wait_for_commands()
+        robotL.wait_for_commands()
+        robotR.wait_until_done()
+        robotL.wait_until_done()
+
+        pL, _, _ = robotL.get_grasping_data()
+        pR, _, _ = robotR.get_grasping_data()
+        robotL.other_robot_grasp_point = np.array(pR)
+        robotR.other_robot_grasp_point = np.array(pL)
+
+        time.sleep(0.1)
+
+        robotR.control_to_target(
+            reference_force=reference_force,
+            distance_cap=1.5,
+            timeout=17,
+            trajectory=trajectory,
+            deadzone_threshold=deadzone_threshold,
+        )
+
+        robotL.control_to_target(
+            reference_force=reference_force,
+            distance_cap=1.5,
+            timeout=17,
+            trajectory=trajectory,
+            deadzone_threshold=deadzone_threshold,
+        )
+
+        robotR.wait_for_control()
+        robotL.wait_for_control()
+
+        robotR.plot_data3D()
+        robotL.plot_data3D()
+
+        robotR.save_everything("pick_and_place_right")
+        robotL.save_everything("pick_and_place_left")
+
+    except KeyboardInterrupt:
+        print("\nInterrupted by user")
+        robotR.stop_control()
+        robotL.stop_control()
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        robotR.stop_control()
+        robotL.stop_control()
+    finally:
+        robotR.disconnect()
+        robotL.disconnect()
+        print("Robot disconnected")
